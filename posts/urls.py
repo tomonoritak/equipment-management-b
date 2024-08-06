b94b9d37dd5b628f9cd77bcc252b35c77992e6f1
tomonoritak <tomonori-takasaki@tokyu-hl.jp>
2024-08-06 23:49:17 +0900

    URL

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,itemdetailsView,itemeditView,itemlistView,itemregistrationView
 
 app_name = 'Posts'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('posts/itemdetails/', itemdetailsView.as_view(), name='itemdetails'),
+    path('posts/itemedit/', itemeditView.as_view(), name='itemedit'),
+    path('posts/itemlist/', itemlistView.as_view(), name='itemlist'),
+    path('posts/itemregistration', itemregistrationView.as_view(), name='itemregistration'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,24 @@ class IndexView(ListView):
     model = Posts
     template_name = 'posts/index.html'
     context_object_name = 'posts'
+
+class itemdetailsView(ListView):
+    model = Posts
+    template_name = 'posts/itemdetails.html'
+    context_object_name = 'itemdetails'
+
+
+class itemeditView(ListView):
+    model = Posts
+    template_name = 'posts/itemedit.html'
+    context_object_name = 'itemedit'
+
+class itemlistView(ListView):
+    model = Posts
+    template_name = 'posts/itemlis.html'
+    context_object_name = 'itemlis'
+
+class itemregistrationView(ListView):
+    model = Posts
+    template_name = 'posts/itemregistration.html'
+    context_object_name = 'itemregistration'

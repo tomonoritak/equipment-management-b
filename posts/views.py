6d9123af9d840f353f79c78f3bf0e1e0cb622838
tomonoritak <tomonori-takasaki@tokyu-hl.jp>
2024-08-08 11:08:21 +0900

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -16,15 +16,11 @@ class itemlistView(ListView):
     context_object_name = 'itemlist'
 
 
-class itemeditView(ListView):
-    model = Posts
-    template_name = 'posts/itemedit.html'
-    context_object_name = 'itemedit'
+#class itemeditView(ListView):
+#    model = Posts
+#    template_name = 'posts/itemedit.html'
+#    context_object_name = 'itemedit'
 
-class itemlistView(ListView):
-    model = Posts
-    template_name = 'posts/itemlis.html'
-    context_object_name = 'itemlis'
 
 class itemCreateView(CreateView):
     form_class = PostForm
@@ -42,21 +38,21 @@ class itemDetailView(DetailView):
     context_object_name = 'post'
 
 
-class loginView(ListView):
-    model = Posts
-    template_name = 'posts/login.html'
-    context_object_name = 'login'
-class usereditView(ListView):
-    model = Posts
-    template_name = 'posts/useredit.html'
-    context_object_name = 'usereditView'
-class userlistView(ListView):
-    model = Posts
-    template_name = 'posts/userlist.html'
-    context_object_name = 'userlist'
-class userregistrationView(ListView):
-    model = Posts
-    template_name = 'posts/userregistration.html'
-    context_object_name = 'userregistration'
+#class loginView(ListView):
+#    model = Posts
+#    template_name = 'posts/login.html'
+#    context_object_name = 'login'
+#class usereditView(ListView):
+#    model = Posts
+#    template_name = 'posts/useredit.html'
+#    context_object_name = 'usereditView'
+#class userlistView(ListView):
+#    model = Posts
+#    template_name = 'posts/userlist.html'
+#    context_object_name = 'userlist'
+#class userregistrationView(ListView):
+#    model = Posts
+#    template_name = 'posts/userregistration.html'
+#    context_object_name = 'userregistration'
 
 

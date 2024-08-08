c0541bed7188acd760243bd97ef370626c6a7a6a
tomonoritak <tomonori-takasaki@tokyu-hl.jp>
2024-08-08 13:57:38 +0900

    修正

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import IndexView, itemCreateView, itemlistView, itemDetailView
+from .views import IndexView, itemCreateView, itemlistView, itemDetailView, itemeditView,orderhistoryView
 
 app_name = 'Posts'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('itemlist/', itemlistView.as_view(), name='itemlist'),
+    path('itemlist/orderhistory/', orderhistoryView.as_view(), name='orderhistory'),
     path('itemlist/create/', itemCreateView.as_view(), name='itemregistration'),    
     path('itemlist/<int:pk>/', itemDetailView.as_view(), name='itemdetail'),
+    path('itemlist/<int:pk>/edit', itemeditView.as_view(), name='itemedit'),
 ]

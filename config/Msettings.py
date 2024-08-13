8ec5b9198f1a6bdc785e489d212dc53581133f74
tomonoritak <tomonori-takasaki@tokyu-hl.jp>
2024-08-13 18:07:30 +0900

    在庫数変更に対応させた

--- config/Msettings.py
+++ config/Msettings.py
@@ -1,20 +1,23 @@
 from pathlib import Path
 import os 
-
+import environ 
+from decouple import config # デプロイ用
+from dj_database_url import parse as dburl # デプロイ用
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@5kwfiweot@_3qq(*jj6trt7y910g#oq&sjyjcc=ou%8u)uq$h'
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'mysite-91sh.onrender.com']
 
 
 # Application definition
@@ -32,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,16 +68,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
+default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'equipment-management-b',
-        'USER': 'postgres',
-        'PASSWORD': '    ', #ご自身の設定したパスワードを設定しましょう
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
 }
 
 
@@ -117,3 +114,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# //本番環境//
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView,CreateView,DetailView,UpdateView
 from django.urls import reverse_lazy
 from .models import Posts
-from .forms import PostForm
+from .forms import PostForm,StockQuantityForm
 from django.urls import reverse
+from django.contrib import messages
+from django.views.generic.edit import FormView
+
 
 class IndexView(ListView):
     model = Posts
@@ -48,3 +51,22 @@ class itemeditView(UpdateView):
 
     def get_success_url(self):
         return reverse('Posts:itemdetail', kwargs={'pk': self.object.pk})
+    
+class StockQuantityUpdateView(FormView):
+    form_class = StockQuantityForm
+    template_name = 'posts/itemdetail.html'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Posts, pk=self.kwargs['pk'])
+        new_stock_quantity = form.cleaned_data['stock_quantity']
+        post.stock_quantity = new_stock_quantity
+        post.save()
+        
+        # Optionally, save stock change history here
+
+        messages.success(self.request, "在庫数が更新されました。")
+        return redirect('Posts:itemdetail', pk=post.pk)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "フォームにエラーがあります。")
+        return self.form_invalid(form)

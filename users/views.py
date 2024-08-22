d0013f5b9a27acbb3414439f516fa13766facbff
1027INA <mikiko-inayoshi@tokyu-hl.jp>
2024-08-22 15:43:43 +0900

    ユーザー一覧と詳細

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.views.generic import ListView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
@@ -24,3 +28,17 @@ class SignUpView(generic.CreateView):
         user.save()
         
         return super().form_valid(form)
+    
+class UserListView(ListView):
+    model = User
+    template_name = 'users/userlist.html'
+    context_object_name = 'users'
+
+class UserEditView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'users/useredit.html'
+    fields = ['username', 'email', 'is_staff', 'is_superuser']  # 編集したいフィールドを指定
+    success_url = reverse_lazy('users:userlist')  # 保存後にユーザー一覧にリダイレクト
+
+    def get_object(self, queryset=None):
+        return User.objects.get(pk=self.kwargs['pk'])  # プライマリキーでユーザーを取得
